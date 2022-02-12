1ba076d172621b114bff9937cad5f569e6ea72c5
chathu_08 <chathu.rash97@gmail.com>
2022-02-12 07:22:21 +0530

    Completed the newsletter component

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -7,6 +7,13 @@ const Newsletter = () => (
   <div className='app__newsletter'>
     <div className='app__newsletter-heading'>
       <SubHeading title='Newsletter' />
+      <p className="p__opensans">And never miss latest Updates!</p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      
+      <button type="button" className="custom__button">Subscribe</button>
     </div>
   </div>
 );
