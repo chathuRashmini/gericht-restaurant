278272e3447a6f045e150fc0780067841e7c7a5f
chathu_08 <chathu.rash97@gmail.com>
2022-02-12 07:20:36 +0530

    Completed footer overlay component

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './Newsletter.css';
+import SubHeading from '../SubHeading/SubHeading';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title='Newsletter' />
+    </div>
   </div>
 );
 
